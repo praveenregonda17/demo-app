8c163639e69cb3671fa5efa683cd6ebdaa77a0a9
Praveen Regonda <praveenregonda@sumits-MacBook-Pro.local>
2024-04-15 09:54:01 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import axios from 'axios';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState([])
-  axios.get(`${process.env.REACT_APP_API_URL}/v0.1/countries`)
-  .then(function (response) {
-    setData(response.data.data)
-  })
-  .catch(function (error) {
-    setData([])
-  })
-  .finally(function () {
-  });
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/v0.1/countries`)
+      .then(function (response) {
+        setData(response.data.data)
+      })
+      .catch(function (error) {
+        setData([])
+      })
+      .finally(function () {
+      });
+  }, [])
   console.log('data', data)
   return (
     <div className="App">
-     Demo Test App
-     <ul>
-      {data.map((item: any) => <li>{item.country}</li>)}
+      Demo Test App
+      <ul>
+        {data.map((item: any) => <li>{item.country}</li>)}
       </ul>
     </div>
   );
